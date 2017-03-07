a94e48b859b8cfaee000d8d8957c1ed185a2e006
Juraj Martinka <jumarko@gmail.com>
2017-03-07 10:06:35 +0100

    Add separate table for authors.
    New queries added to the queries.sql.

---
+++ resources/migrations/20170307-add-authors-table.up.sql
@@ -0,0 +1,4 @@
+CREATE TABLE authors
+(id VARCHAR(20) PRIMARY KEY,
+ first_name VARCHAR(30),
+ last_name VARCHAR(60));
--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -25,3 +25,30 @@ WHERE id = :id
 -- :doc delete a book given the id
 DELETE FROM books
 WHERE id = :id
+
+
+-- :name create-author! :! :n
+-- :doc persists a new author
+INSERT INTO authors
+(id, first_name, last_name)
+VALUES (:id, :first-name, :last-name)
+
+-- :name update-author! :! :n
+-- :doc update an existing author
+UPDATE authors
+SET first_name = :first-name, last_name = :last-name
+WHERE id = :id
+
+-- :name get-authors :? :*
+-- :doc retrieve all authors
+SELECT * FROM authors
+
+-- :name get-author :? :1
+-- :doc retrieve an author given the id.
+SELECT * FROM authors
+WHERE id = :id
+
+-- :name delete-author! :! :n
+-- :doc delete an author given the id
+DELETE FROM authors
+WHERE id = :id
