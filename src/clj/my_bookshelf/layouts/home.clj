61d56f2ab4346273d1e6531399ca8e6d1878ac09
Juraj Martinka <jumarko@gmail.com>
2017-03-08 09:52:45 +0100

    Add "Add Book" button to the home page.
    The skeleton for add-book action template has been added.
    However, it's just a copy&paste from luminus guestbook application.

--- src/clj/my_bookshelf/layouts/home.clj
+++
@@ -1,7 +0,0 @@
-(ns my-bookshelf.layouts.home
-  (:require
-   [my-bookshelf.layout :as l]))
-
-(defn render-home [books]
-  (l/render "home.html"  {:books books}))
-
--- src/clj/my_bookshelf/routes/home.clj
+++ src/clj/my_bookshelf/routes/home.clj
@@ -1,13 +1,12 @@
 (ns my-bookshelf.routes.home
-  (:require
-   [clojure.java.io :as io]
-   [compojure.core :refer [defroutes GET POST]]
-   [java-time :as t]
-   [my-bookshelf.db.core :as db]
-   [my-bookshelf.layouts.home :as l]
-   [my-bookshelf.model.entities :as m]
-   [ring.util.http-response :as response]
-   [my-bookshelf.utils.date-utils :refer [to-date]]))
+  (:require [clojure.java.io :as io]
+            [compojure.core :refer [defroutes GET POST]]
+            [java-time :as t]
+            [my-bookshelf.db.core :as db]
+            [my-bookshelf.layout :as l]
+            [my-bookshelf.model.entities :as m]
+            [my-bookshelf.utils.date-utils :refer [to-date]]
+            [ring.util.http-response :as response]))
 
 (defn save-book!
   "Persists new boook"
@@ -16,8 +15,12 @@
 )
 
 (defn home-page []
-  (l/render-home (m/books-with-authors (db/get-books-with-authors))))
+  (l/render "home.html"  {:books (m/books-with-authors (db/get-books-with-authors))}))
+
+(defn add-book-page []
+  (l/render "add-book.html"))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
+  (GET "/add-book" [] (add-book-page))
   (POST "/books" request (save-book! request)))
